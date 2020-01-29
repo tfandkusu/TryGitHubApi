1ba39cd35009a714251517682f63c2fa7c81458f
Haruhiko Takada <haruhiko.takada@gmail.com>
2020-01-29 22:57:33 +0900

    リポジトリを取得

--- trygithubapi/main.py
+++ trygithubapi/main.py
@@ -1,3 +1,10 @@
+import os
+from github import Github
+
 
 def main():
+    access_token = os.environ['BITRISEIO_GITHUB_API_ACCESS_TOKEN']
+    g = Github(access_token)
+    repo = g.get_user().get_repo("quickecho")
+    print(repo.name)
     print("Hello World")
